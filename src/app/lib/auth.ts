e636d48a7d42956bc7cfe3d4ce9a52f2e504fd36
nurulamin092 <mnurulamin092@gmail.com>
2026-03-14 15:18:42 +0600

    login register done

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -1,13 +1,17 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { Prisma, Role, UserStatus } from "../../generated/prisma/client";
+import { Role, UserStatus } from "../../generated/prisma/enums";
+import { prisma } from "./prisma";
+
 export const auth = betterAuth({
-  database: prismaAdapter(Prisma, {
+  database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+
   emailAndPassword: {
     enabled: true,
   },
+
   user: {
     additionalFields: {
       role: {
--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { catchAsync } from "../../shared /catchAsync";
-import { sendResponse } from "../../shared /sendResponse";
+import { catchAsync } from "../../shared/catchAsync";
+import { sendResponse } from "../../shared/sendResponse";
 import { AuthService } from "./auth.service";
 
 const registerPatient = catchAsync(async (req: Request, res: Response) => {
