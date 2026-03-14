27fb2b29d6874473e0f3eeeba0f4fd6b85f8d16f
nurulamin092 <mnurulamin092@gmail.com>
2026-03-14 09:13:11 +0600

    prisma set up

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express, { Application, Request, Response } from "express";
+import { prisma } from "./app/lib/prisma";
+
+const app: Application = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+// app.use("/api/v1", IndexRoutes);
+
+app.get("/", async (req: Request, res: Response) => {
+  const specialty = await prisma.specialty.create({
+    data: {
+      title: "Cardiology",
+    },
+  });
+  res.status(201).json({
+    success: true,
+    message: "API is working",
+    data: specialty,
+  });
+});
+
+export default app;
